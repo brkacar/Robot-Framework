fa85889862e747ce3b63b170a308653a5ea4fe30
Berk Acar <brkacaran@gmail.com>
2026-04-05 16:57:06 +0200

    Fix shop.py

--- CustomLibraries/Shop.py
+++ CustomLibraries/Shop.py
@@ -5,19 +5,25 @@ from robot.libraries.BuiltIn import BuiltIn
 class Shop:
     def __init__(self):
         self.seleniumLib = BuiltIn().get_library_instance("SeleniumLibrary")
-
+        self.builtin = BuiltIn()
 
     @keyword
-    def Add_Items_to_Card_and_Checkout(self, *itemsList): # * catches unpacked list as tuple
-        i=1
+    def Add_Items_to_Card_and_Checkout(self, *itemsList):
         self.seleniumLib.wait_until_element_is_visible("css:.card-title", "10s")
-        self.seleniumLib.wait_until_element_is_visible("css:li.active a", "10s")
-        #Get WebElements
-        items=self.seleniumLib.get_webelements("css=.card-title")
-        for item in items:
-            if item.text in itemsList:
-                self.seleniumLib.click_button("(//*[ @class='card-footer'])["+str(i)+"]/button")
-            i=i+1
 
-        self.seleniumLib.click_link("css:li.active a")
+        for item_name in itemsList:
+            button_locator = (
+                f"xpath://h4[@class='card-title' and normalize-space()='{item_name}']"
+                f"/ancestor::div[contains(@class,'card')]//button"
+            )
+            self.seleniumLib.wait_until_element_is_visible(button_locator, "10s")
+            self.seleniumLib.scroll_element_into_view(button_locator)
+            self.builtin.wait_until_keyword_succeeds(
+                "10s", "1s", "Click Element", button_locator
+            )
 
+        cart_locator = "css:li.active a"
+        self.seleniumLib.wait_until_element_is_visible(cart_locator, "10s")
+        self.builtin.wait_until_keyword_succeeds(
+            "10s", "1s", "Click Element", cart_locator
+        )
