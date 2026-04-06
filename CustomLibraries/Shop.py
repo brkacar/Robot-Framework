15937e85a6377366778d4e8ebe071d92d4206bd9
Berk Acar <96752602+brkacar@users.noreply.github.com>
2026-04-06 08:26:42 +0200

    fix: rewrite Shop.py with correct ProtoCommerce locators - card-title, card-footer button, Checkout link

--- CustomLibraries/Shop.py
+++ CustomLibraries/Shop.py
@@ -10,36 +10,26 @@ class Shop:
 
     @keyword("Add Items To Card And Checkout")
     def Add_Items_to_Card_and_Checkout(self, *itemsList):
-        # Wait for all ADD TO CART buttons to be present and interactable
-        self.seleniumLib.wait_until_element_is_visible("css:.card-footer button", "15s")
-        self.seleniumLib.wait_until_element_is_enabled("css:.card-footer button", "15s")
+        # Site: rahulshettyacademy.com/angularpractice/shop (ProtoCommerce)
+        # Wait for cards to load
+        self.seleniumLib.wait_until_element_is_visible("css:.card-title", "15s")
         time.sleep(1)
 
-        # Get card titles and click matching ADD TO CART buttons
-        items = self.seleniumLib.get_webelements("css:.card-title")
-        for i, item in enumerate(items, start=1):
-            if item.text in itemsList:
-                btn_locator = "xpath:(//*[@class='card-footer'])[" + str(i) + "]/button"
-                self.builtin.wait_until_keyword_succeeds(
-                    "15s", "1s", "Click Button", btn_locator
-                )
+        # Get all card title and button elements by index
+        title_elements = self.seleniumLib.get_webelements("css:.card-title")
+        add_buttons = self.seleniumLib.get_webelements("css:.card-footer button")
 
-        # Click the cart/checkout link - try multiple locators for robustness
-        cart_locators = [
-            "xpath://a[.//img[@alt='Cart']]",
-            "xpath://a[contains(@href,'#') and .//img]",
-            "css:a img[alt='Cart']",
-            "css:img[alt='Cart']",
-        ]
-        clicked = False
-        for locator in cart_locators:
-            try:
-                self.seleniumLib.wait_until_element_is_visible(locator, "5s")
-                self.seleniumLib.scroll_element_into_view(locator)
-                self.seleniumLib.click_element(locator)
-                clicked = True
-                break
-            except Exception:
-                continue
-        if not clicked:
-            raise AssertionError("Could not find or click the cart icon to proceed to checkout")
+        for i, title_el in enumerate(title_elements):
+            title_text = title_el.text.strip()
+            if title_text in itemsList:
+                self.builtin.wait_until_keyword_succeeds(
+                    "15s", "1s", "Click Element", add_buttons[i]
+                )
+                time.sleep(0.5)
+
+        # Click the Checkout link in the navbar
+        checkout_locator = "xpath://a[contains(.,'Checkout')]"
+        self.seleniumLib.wait_until_element_is_visible(checkout_locator, "15s")
+        self.builtin.wait_until_keyword_succeeds(
+            "15s", "1s", "Click Element", checkout_locator
+        )
