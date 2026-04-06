c868f0280701b30882df1482ef1b0eb3898b9bc8
Berk Acar <96752602+brkacar@users.noreply.github.com>
2026-04-06 08:17:54 +0200

    fix: wait for ADD TO CART buttons to be visible and enabled before clicking
    Refactor item addition and checkout process to include waiting for elements and improved button clicking logic.

--- CustomLibraries/Shop.py
+++ CustomLibraries/Shop.py
@@ -1,5 +1,6 @@
 from robot.api.deco import keyword, library
 from robot.libraries.BuiltIn import BuiltIn
+import time
 
 @library
 class Shop:
@@ -9,14 +10,24 @@ class Shop:
 
     @keyword("Add Items To Card And Checkout")
     def Add_Items_to_Card_and_Checkout(self, *itemsList):
-        # Get WebElements and add matching items to cart
-        i = 1
+        # Wait for all ADD TO CART buttons to be present and interactable
+        self.seleniumLib.wait_until_element_is_visible("css:.card-footer button", "15s")
+        self.seleniumLib.wait_until_element_is_enabled("css:.card-footer button", "15s")
+        time.sleep(1)
+
+        # Get card titles and click matching ADD TO CART buttons
         items = self.seleniumLib.get_webelements("css:.card-title")
-        for item in items:
+        for i, item in enumerate(items, start=1):
             if item.text in itemsList:
-                self.seleniumLib.click_button("xpath:(//*[@class='card-footer'])["+str(i)+"]/button")
+                btn_locator = "xpath:(//*[@class='card-footer'])[" + str(i) + "]/button"
+                self.builtin.wait_until_keyword_succeeds(
+                    "15s", "1s", "Click Button", btn_locator
+                )
 
-            i = i + 1
-
-        # Click the checkout
-        self.seleniumLib.click_link("css:li.active a")
+        # Click the cart icon to proceed to checkout
+        cart_locator = "css:img[alt='Cart']"
+        self.seleniumLib.wait_until_element_is_visible(cart_locator, "15s")
+        self.seleniumLib.scroll_element_into_view(cart_locator)
+        self.builtin.wait_until_keyword_succeeds(
+            "15s", "2s", "Click Element", cart_locator
+        )
